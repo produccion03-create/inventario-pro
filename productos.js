4b210085279f4ece3ee3037b87dbfb256d0eeb08
produccion03-create <produccion03@karey.com>
2026-07-14 09:11:22 +0200

    Add files via upload

---
+++ productos.js
@@ -0,0 +1,74 @@
+import { db, collection, addDoc, getDocs } from "./firebase.js";
+
+async function guardarProducto() {
+
+    let codigo = document.getElementById("codigo").value.trim();
+    let nombre = document.getElementById("nombre").value.trim();
+    let stock = document.getElementById("stock").value.trim();
+    let ubicacion = document.getElementById("ubicacion").value.trim();
+    let precio = document.getElementById("precio").value.trim();
+
+    if (codigo === "" || nombre === "") {
+        alert("Introduce al menos el código y el nombre.");
+        return;
+    }
+
+    try {
+
+        await addDoc(collection(db, "productos"), {
+            codigo,
+            nombre,
+            stock: Number(stock),
+            ubicacion,
+            precio: Number(precio)
+        });
+
+        alert("✅ Producto guardado en Firebase");
+
+        document.getElementById("codigo").value = "";
+        document.getElementById("nombre").value = "";
+        document.getElementById("stock").value = "";
+        document.getElementById("ubicacion").value = "";
+        document.getElementById("precio").value = "";
+
+        mostrarProductos();
+
+    } catch (error) {
+
+        console.error(error);
+        alert("Error al guardar el producto");
+
+    }
+
+}
+
+async function mostrarProductos() {
+
+    let lista = document.getElementById("lista");
+
+    lista.innerHTML = "";
+
+    const querySnapshot = await getDocs(collection(db, "productos"));
+
+    querySnapshot.forEach((doc) => {
+
+        const p = doc.data();
+
+        lista.innerHTML += `
+        <div>
+            📦 <b>${p.nombre}</b><br>
+            Código: ${p.codigo}<br>
+            Stock: ${p.stock}<br>
+            Ubicación: ${p.ubicacion}<br>
+            Precio: ${p.precio} €
+            <hr>
+        </div>
+        `;
+
+    });
+
+}
+
+window.guardarProducto = guardarProducto;
+
+mostrarProductos();
