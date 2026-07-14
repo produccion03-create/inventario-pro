81a6edcc90db414ba1a580d60b79fc37b711c5c7
produccion03-create <produccion03@karey.com>
2026-07-14 13:01:22 +0200

    Add files via upload

--- JS/productos.js
+++ JS/productos.js
@@ -3,9 +3,17 @@ console.log("PRODUCTOS.JS CARGADO");
 import { db, collection, addDoc, getDocs } from "./firebase.js";
 
 async function guardarProducto() {
-    alert("EL BOTÓN FUNCIONA");
-}
 
+    let codigo = document.getElementById("codigo").value.trim();
+    let nombre = document.getElementById("nombre").value.trim();
+    let stock = document.getElementById("stock").value.trim();
+    let ubicacion = document.getElementById("ubicacion").value.trim();
+    let precio = document.getElementById("precio").value.trim();
+
+    if (codigo === "" || nombre === "") {
+        alert("Introduce el código y el nombre.");
+        return;
+    }
 
     try {
 
